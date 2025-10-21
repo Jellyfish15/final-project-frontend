159a5905d404b825c1a1de6e05f7df4ad2659ed1
Gennadiy Strelkov <jellyfishbountyhunter@gmail.com>
2025-10-20 20:18:54 -0400

    Nudl app Building. Update the app to be 100% grade value for TripleTen. Also create an interface that is better suited for Iphone to function more like an app than a website.

---
+++ src/components/BottomNavigation/BottomNavigation.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './BottomNavigation.css';
+
+const BottomNavigation = () => {
+  const location = useLocation();
+
+  const navItems = [
+    {
+      path: '/videos',
+      icon: '🎥',
+      label: 'Videos',
+      isActive: location.pathname === '/videos'
+    },
+    {
+      path: '/search',
+      icon: '🔍',
+      label: 'Search',
+      isActive: location.pathname === '/search'
+    },
+    {
+      path: '/profile',
+      icon: '👤',
+      label: 'Profile',
+      isActive: location.pathname === '/profile'
+    }
+  ];
+
+  return (
+    <nav className="bottom-nav">
+      <div className="bottom-nav__container">
+        {navItems.map((item) => (
+          <Link
+            key={item.path}
+            to={item.path}
+            className={`bottom-nav__item ${
+              item.isActive ? 'bottom-nav__item--active' : ''
+            }`}
+          >
+            <span className="bottom-nav__icon">{item.icon}</span>
+            <span className="bottom-nav__label">{item.label}</span>
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default BottomNavigation;
